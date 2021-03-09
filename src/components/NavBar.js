64d34e148cd9739dbc4522ecbdf1936b28721f8a
rinio <r.nelstein@live.nl>
2021-03-09 09:20:12 +0100

    fetch random quote
    - Fetch quotes from an external service (eg: http://quotes.stormconsultancy.co.uk/api)
    - unit test

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+export function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Kabisa coding assignment</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ src/quotesAPI.test.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { getRandomQuote } from "./quotesAPI";
+
+jest.mock("axios");
+
+it("returns qu title of the first album", async () => {
+  axios.get.mockResolvedValue({
+    data: {
+      author: "Alan Kay",
+      id: 38,
+      permalink: "http://quotes.stormconsultancy.co.uk/quotes/38",
+      quote:
+        "Most software today is very much like an Egyptian pyramid with millions of bricks piled on top of each other, with no structural integrity, but just done by brute force and thousands of slaves.",
+    },
+  });
+
+  const { data } = await getRandomQuote();
+  
+  const expected = 38;
+
+  expect(data.id).toEqual(expected);
+});
